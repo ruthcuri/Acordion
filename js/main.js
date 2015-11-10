cc07235ddbb2c4dc8bc9f7697fd90ba293bdcbb1
unknown <curimelgarejo@gmail.com>
2015-11-10 18:06:30 +0100

    acordion de imagenes

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	
+	<script type=”text/javascript”><br />
+$().ready(function() {<br />
+$(‘.jimgMenu ul’).kwicks({max: 310, duration: 300, easing: ‘easeOutQuad’, complete: ‘callback’});<br />
+});<br />
+</script><br />
+	
+});
